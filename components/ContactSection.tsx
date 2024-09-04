075ef44cd636f68d316f35f6cb41060c9a363e2e
Denys Koshevko <121438170+dkoshevko@users.noreply.github.com>
2024-09-04 16:12:40 +0200

    contact resp.

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -4,9 +4,9 @@ import { ContactForm } from "./ContactForm";
 export default function ContactSection() {
   return (
     <section id="contact" className="gradient-bg flex flex-col items-center py-16">
-      <h2 className="uppercase text-3xl tracking-wide font-semibold mb-10">Parlez-nous de votre projet</h2>
+      <h2 className="container uppercase text-center text-3xl tracking-wide font-semibold mb-10">Parlez-nous de votre projet</h2>
       <div className="container flex justify-center">
-        <ContactForm customClass="grid w-1/2 space-y-3" />
+        <ContactForm customClass="grid w-full lg:w-1/2 space-y-3" />
       </div>
     </section>
   );
